ea74c90acb0a42ae2c962ed4fe59bbb5d30326a6
Fernando Helfenstein <fc2con@gmail.com>
2026-08-24 20:18:59 -0300

    Aula 18 - finalizando componente Heading e tipagem TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading attr={123} attr2='String'>
+      <Heading>
         Olá Mundo App - 01
+        <button>
+          <TimerIcon />
+        </button>
       </Heading>
 
       <p>
