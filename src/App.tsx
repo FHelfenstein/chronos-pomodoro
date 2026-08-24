a475a271f680881f18b1ce7e93e5555881a2ea40
Fernando Helfenstein <fc2con@gmail.com>
2026-08-24 19:00:31 -0300

    Aula 14 - Importando e usando arquivos de CSS Modules com escopo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Heading } from './components/Heading';
+
+import './styles/theme.css';
+import './styles/global.css';
+
 export function App() {
   return (
     <>
-      <h1>Olá Mundo (App)</h1>
+      <Heading />
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. At nostrum
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At nostrum
+        </h2>
         dolores eligendi quibusdam aliquam nam culpa incidunt veritatis quos
         veniam! Impedit mollitia earum assumenda, minus rem consequatur ipsam
         explicabo temporibus.
---
+++ src/components/Heading.tsx
@@ -0,0 +1,7 @@
+import styles from './Heading.module.css';
+
+export function Heading() {
+  return (
+    <h1 className={`${styles.heading} ${styles.cyan}`}>Olá Mundo (App)</h1>
+  );
+}
