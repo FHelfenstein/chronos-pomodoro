443f58496f2eb4da8d91430355e0f480a00635d3
Fernando Helfenstein <fc2con@gmail.com>
2026-08-24 21:38:55 -0300

    Aula 21 - Criando a estrutura HTML e CSS do nosso App para cada seção do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,29 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import {
-  HomeIcon,
-  RotateCcwClock,
-  SettingsIcon,
-  SunIcon,
-  TimerIcon,
-} from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <HomeIcon />
-      <RotateCcwClock />
-      <SettingsIcon />
-      <SunIcon />
-
-      <Heading>
-        Olá Mundo App - 01
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. At nostrum
-        dolores eligendi quibusdam aliquam nam culpa incidunt veritatis quos
-        veniam! Impedit mollitia earum assumenda, minus rem consequatur ipsam
-        explicabo temporibus.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
