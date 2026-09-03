020eedb87380173798aabf72b575ab049364451c
Fernando Helfenstein <fc2con@gmail.com>
2026-09-02 22:16:46 -0300

    Aula 50 - Organizando os contextos em arquivos separados

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,40 +2,10 @@ import { PlayCircleIcon } from 'lucide-react';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { Cycles } from '../Cycles';
-import { useTaskContext } from '../../contexts/TaskContext';
 
 export function MainForm() {
-  const { setState } = useTaskContext();
-  //export function MainForm({ state, setState }: HomeProps) {
-  /*  Exemplo aonde passando props atualizamos o estado do objeto em um terceiro nível ,aqui sem uso de contextAPI
-  function handleClick() {
-    setState(prevState => {
-      return {
-        ...prevState,
-        config: {
-          ...prevState.config,
-          workTime: 34,
-        },
-        formattedSecondsRemaining: '23:34',
-      };
-    });
-  }
-  */
-
-  function handleClick() {
-    setState(prevState => {
-      return {
-        ...prevState,
-        formattedSecondsRemaining: '21:35',
-      };
-    });
-  }
-
   return (
     <form className='form' action=''>
-      <button type='button' onClick={handleClick}>
-        Alterar Horário
-      </button>
       <div className='formRow'>
         <DefaultInput
           id='input'
@@ -60,3 +30,19 @@ export function MainForm() {
     </form>
   );
 }
+
+//export function MainForm({ state, setState }: HomeProps) {
+/*  Exemplo aonde passando props atualizamos o estado do objeto em um terceiro nível ,aqui sem uso de contextAPI
+  function handleClick() {
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: {
+          ...prevState.config,
+          workTime: 34,
+        },
+        formattedSecondsRemaining: '23:34',
+      };
+    });
+  }
+  */
