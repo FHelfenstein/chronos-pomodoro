4a51d4a4b9f1fd8a99bf28080d6cc83c7d584eac
Fernando Helfenstein <fc2con@gmail.com>
2026-09-11 16:08:25 -0300

    Aula 82 até 86 - Página History - dados das Tasks em Table

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,10 +1,69 @@
+import { Trash2 } from 'lucide-react';
+import { Container } from '../../components/Container';
+import { DefaultButton } from '../../components/DefaultButton';
 import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
 
+import styles from './styles.module.css';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { formatDate } from '../../utils/formatDate';
+import { getTaskStatus } from '../../utils/getTaskStatus';
+
 export function History() {
+  const { state } = useTaskContext();
+
   return (
     <MainTemplate>
-      <Heading>Página de histórico</Heading>
+      <Container>
+        <Heading>
+          <span>History</span>
+          <span className={styles.buttonContainer}>
+            <DefaultButton
+              icon={<Trash2 />}
+              color='red'
+              aria-label='Apagar todo o histórico'
+              title='Apagar histórico'
+            />
+          </span>
+        </Heading>
+      </Container>
+
+      <Container>
+        <div className={styles.responsiveTable}>
+          <table>
+            <thead>
+              <tr>
+                <th>Tarefa</th>
+                <th>Duração</th>
+                <th>Data</th>
+                <th>Status</th>
+                <th>Tipo</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {/*Array.from({ length: 20 }).map((_, index) => {}*/}
+              {state.tasks.map(task => {
+                const taskTypeDictionary = {
+                  workTime: 'Foco',
+                  shortBreakTime: 'Descanso curto',
+                  longBreakTime: 'Descanso longo',
+                };
+
+                return (
+                  <tr key={task.id}>
+                    <td>{task.name}</td>
+                    <td>{task.duration}min</td>
+                    <td>{formatDate(task.startDate)}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
+                    <td>{taskTypeDictionary[task.type]}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </Container>
     </MainTemplate>
   );
 }
